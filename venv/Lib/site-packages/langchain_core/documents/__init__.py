18ca4d7898285e794fbf8a19cf2ea32711b70229
sampriti.tech <sampritighosh2701@gmail.com>
2024-07-03 21:54:08 +0530

    First commit

---
+++ venv/Lib/site-packages/langchain_core/documents/__init__.py
@@ -0,0 +1,8 @@
+"""**Document** module is a collection of classes that handle documents
+and their transformations.
+
+"""
+from langchain_core.documents.base import Document
+from langchain_core.documents.transformers import BaseDocumentTransformer
+
+__all__ = ["Document", "BaseDocumentTransformer"]
